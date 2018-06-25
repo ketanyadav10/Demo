e12b5635360368b8076f34b01524336b310e0431
ketan <kyadav073@gmail.com>
2018-06-25 21:50:09 +0530

    Initial Commit

---
+++ src/main/java/org/ketone/practice/Testing.java
@@ -0,0 +1,10 @@
+package org.ketone.practice;
+
+public class Testing {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World , Info");
+        System.out.println("Hello World , Error");
+    }
+}
